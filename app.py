c375dd790b17464fd3472f71ce9769f9ac803727
Liu Grace <liugrace@Lius-MacBook-Pro.local>
2023-10-16 01:22:35 +0100

    Update:

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from utils import helpers
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
 
 
 
